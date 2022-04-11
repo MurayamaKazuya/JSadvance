1ba990750e5a8d197b3909a073462bf56f617c29
MurayamaKazuya <75463826+MurayamaKazuya@users.noreply.github.com>
2022-04-11 21:18:48 +0900

    second

--- PageScroll/js/script.js
+++ PageScroll/js/script.js
@@ -1,11 +1,10 @@
 $(function () {
-  $('a[href^="#"]').click(function () {
+  $('[data-scroll]').click(function () {
     const speed = 400;
     const href = $(this).attr("href");
-    const target = $(href == "#" || href == "" ? 'html' : href);
+    const target = $(href === "#" || href === "" ? 'html' : href);
     $('body,html').animate({
       scrollTop: target.offset().top
     }, speed, 'swing');
-    return false;
   });
 });
