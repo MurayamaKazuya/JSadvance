e0e3dc4bc6ef4bbe329ffdd3cce062527cc4d4fb
MurayamaKazuya <75463826+MurayamaKazuya@users.noreply.github.com>
2022-04-14 21:26:39 +0900

    redo

--- PageScroll/js/script.js
+++ PageScroll/js/script.js
@@ -1,8 +1,8 @@
 $(function () {
-  $('[data-scroll]').click(function () {
+  $('a').click(function () {
     const speed = 400;
-    const href = $(this).attr("href");
-    const target = $(href === "#" || href === "" ? 'html' : href);
+    const scroll = $(this).data('scroll');
+    const target = $('div[data-scroll="' + scroll + '"]');
     $('body,html').animate({
       scrollTop: target.offset().top
     }, speed, 'swing');
